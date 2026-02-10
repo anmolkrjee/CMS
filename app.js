292b2c49c97b9fdeee03af3856c6662198639cb6
Anmol Kumar <anmolkrjee1@gmail.com>
2026-02-10 09:43:39 +0530

    Second commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the CMS');
+});
+
+
+export default app;
