51d3691693729f73170adcbc3eb631ded3e75b2c
Anmol Kumar <anmolkrjee1@gmail.com>
2026-02-12 10:45:13 +0530

    Added webhooks

---
+++ webhook/webhooks.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/test", (req, res) => {
+    console.log("Github webhook Received");
+    console.log(req.body);
+    res.status(200).json({ 
+        received: true
+    });
+
+    res.json({ received: true });
+});
+
+export default router;
